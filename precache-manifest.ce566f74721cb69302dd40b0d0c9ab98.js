984226877ab42ab179228090cd171b5767bf310a
JaykibyMinhDung <doanminhdung1729@gmail.com>
2023-02-27 00:43:50 +0700

    Updates

---
+++ precache-manifest.ce566f74721cb69302dd40b0d0c9ab98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5566b875a9f3a8e8a8f9eaacb32f4329",
+    "url": "/WebMovies/index.html"
+  },
+  {
+    "revision": "4fc59ae60be27f775ecb",
+    "url": "/WebMovies/static/css/main.2f7e6a9d.chunk.css"
+  },
+  {
+    "revision": "bc2a5dae03e9260a5c14",
+    "url": "/WebMovies/static/js/2.ae110de7.chunk.js"
+  },
+  {
+    "revision": "7bc32abedf93182618c76e1cb46e37e6",
+    "url": "/WebMovies/static/js/2.ae110de7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fc59ae60be27f775ecb",
+    "url": "/WebMovies/static/js/main.ed415ff5.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/WebMovies/static/js/main.ed415ff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efad28edac19c9a351ba",
+    "url": "/WebMovies/static/js/runtime-main.79384fb4.js"
+  }
+]);
